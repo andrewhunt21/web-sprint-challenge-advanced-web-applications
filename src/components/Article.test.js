f9316d4347f3aeb9ec3ab7db33a5fecad4c70161
andrewhunt21 <andrewhunt21@icloud.com>
2021-12-17 16:20:58 -0800

    working on tests

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -5,6 +5,9 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
+import { resetArticles } from '../mocks/data';
+jest.mock('../mocks/data');
+
 import Article from './Article';
 
 test('renders component without errors', ()=> {
@@ -12,16 +15,41 @@ test('renders component without errors', ()=> {
 });
 
 test('renders headline, author from the article when passed in through props', ()=> {
-    render(<Article articles={[]} />);
+    render(<Article headline={"test headline"} author={"test author"} />);
 
     const headline = screen.getByTestId('headline');
+    const author = screen.getByTestId('author');
 
+    expect(headline).toHaveTextContent(/test headline/i);
+    expect(author).toHaveTextContent(/test author/i);
 });
 
 test('renders "Associated Press" when no author is given', ()=> {
+    render(<Article />);
+
+    const author = screen.getByTestId('author')
+
+    expect(author).toHaveTextContent(/Associated Press/i);
 });
 
-test('executes handleDelete when the delete button is pressed', ()=> {
+test('executes handleDelete when the delete button is pressed', async ()=> {
+    resetArticles.mockResolvedValueOnce({
+        articles: [
+            {id: 1,
+                headline: "Test Headline",
+                author:"Test Author",
+                summary: "Test Summary",
+                body: "Test Body"}
+        ]
+    });
+
+    render(<Article />)
+
+    const deleteButton = screen.getByTestId('deleteButton');
+    userEvent.click(deleteButton);
+
+    const articles = await screen.findByTestId("id")
+    expect(articles).toHaveLength(0);
 });
 
 //Task List: 
